631666e29fe56922272b9305e2dcaa4760cee854
MohammadSheakh <mohammad.sheakh@gmail.com>
2023-10-19 14:24:54 +0600

    starting

---
+++ E-Commerce-Web-Application/Controllers/Seller/ProductController.cs
@@ -0,0 +1,38 @@
+﻿// using E_Commerce_Web_Application.Models.Common;
+using E_Commerce_Web_Application.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce_Web_Application.Controllers.Seller
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        [HttpGet]
+        public ActionResult addProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult addProduct(Product product)
+        {
+            return View(product);
+        }
+
+        public ActionResult updateProductDetails()
+        {
+            return View();
+        }
+
+        // chillpc\sqlexpress
+
+        public ActionResult deleteProduct()
+        {
+            return View();
+        }
+    }
+}
---
+++ E-Commerce-Web-Application/Controllers/Seller/SellerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce_Web_Application.Controllers.Seller
+{
+    public class SellerController : Controller
+    {
+        // GET: Seller
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
